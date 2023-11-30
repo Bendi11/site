8da4b1a231597dab7578d78b4be44523739b7e7d
Bendi11 <bkliebmann@gmail.com>
2023-11-29 17:24:09 -0800

    Added test page for backend

--- site-back/src/main.rs
+++ site-back/src/main.rs
@@ -1,7 +1,16 @@
-use rocket::Rocket;
+use rocket::{Rocket, get, routes};
 
+#[get("/index.html")]
+fn index() -> &'static str {
+    "Hello, World!"
+}
 
 #[rocket::launch]
 async fn launch() -> _ {
     Rocket::build()
+        .configure(rocket::Config {
+            port: 9115,
+            ..Default::default()
+        })
+        .mount("/", routes![ index ])
 }
