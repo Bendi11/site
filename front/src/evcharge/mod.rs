11308bbc880a5ca893ed62b4512b0c06f2b29005
Bendi11 <bkliebmann@gmail.com>
2023-11-30 13:10:50 -0800

    Added tesla developer public key to appropriate directory

--- front/src/evcharge/mod.rs
+++ front/src/evcharge/mod.rs
@@ -1,10 +1,13 @@
 use leptos::{component, IntoView, view};
 use leptos_router::Route;
 
+use crate::Navbar;
+
 #[component(transparent)]
 pub fn EvChargeRoutes() -> impl IntoView {
     view! {
         <Route path=".evcharge" view=EvChargeHome>
+            <Route path="index" view=EvChargeHome />
         </Route>
     }
 }
@@ -12,6 +15,7 @@ pub fn EvChargeRoutes() -> impl IntoView {
 #[component]
 pub fn EvChargeHome() -> impl IntoView {
     view! {
+        <Navbar />
         <p class="text-9xl text-sol-100">Hello!</p>
     }
 }
